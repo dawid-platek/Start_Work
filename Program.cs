36f365867a5f5db5c4fabb075d85df149b99e3e7
dawid-platek <dawidplatek.eu.pl@gmail.com>
2024-10-10 23:13:36 +0200

    Add Thread Sleep

--- Program.cs
+++ Program.cs
@@ -22,28 +22,36 @@ namespace OpenWebsitesAndDrive
             // Otwieranie programu desktopowego
             string programPath = @"C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE"; //Microsoft Outlook
             Process.Start(new ProcessStartInfo(programPath) { UseShellExecute = true });
-     
+
+            // Dodanie opóźnienia 12 sekund (12000 milisekund)
+            Thread.Sleep(12000);
+
             // Instalacja aplikacji desktopowej ze Sklepu Microsoft
             string appPackage = "Microsoft.Todos_8wekyb3d8bbwe!App"; // Zastąp "Microsoft.YourApp_8wekyb3d8bbwe!App" odpowiednim PackageFamilyName i AppID
             Process.Start(new ProcessStartInfo("explorer.exe", $"shell:AppsFolder\\{appPackage}") { UseShellExecute = true });
 
-            // Ścieżka do Chrome Proxy i parametry
+            // Dodanie opóźnienia 8 sekund (8000 milisekund)
+            Thread.Sleep(8000);
+
+            // Otwieranie aplikacji webowych, ścieżka do Chrome Proxy i parametry
             string chromeProxyPath = @"C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe";
             string profileDirectory = "--profile-directory=\"Profile 3\"";
-            string appId = "--app-id=femkhbenfobnlelheeafglgnpbpfeejk"; //zarabiajtyleilechcesz.pl
-            string appId2 = "--app-id=ddigokhlodmfdnihjnknlmkihblieiga"; //Dun & Bradstreet
-            string appId3 = "--app-id=kjbdgfilnfhdoflbpgamdcdgpehopbe"; //Kalendarz Google
- 
-            // Otwieranie aplikacji webowych
-            Process.Start(new ProcessStartInfo(chromeProxyPath, $"{profileDirectory} {appId}") { UseShellExecute = true });
+            // Otwieranie zarabiajtyleilechcesz.pl
+            string appId1 = "--app-id=femkhbenfobnlelheeafglgnpbpfeejk";
+            Process.Start(new ProcessStartInfo(chromeProxyPath, $"{profileDirectory} {appId1}") { UseShellExecute = true });
+            // Otwieranie Dun & Bradstreet
+            string appId2 = "--app-id=ddigokhlodmfdnihjnknlmkihblieiga";
             Process.Start(new ProcessStartInfo(chromeProxyPath, $"{profileDirectory} {appId2}") { UseShellExecute = true });
+            // Otwieranie Kalendarz Google
+            string appId3 = "--app-id=kjbdgfilnfhdoflbpgamdcdgpehopbep";
             Process.Start(new ProcessStartInfo(chromeProxyPath, $"{profileDirectory} {appId3}") { UseShellExecute = true });
 
+            // Ścieżka do klasycznej aplikacji Excel
+            string excelAppPath = @"C:\Program Files (x86)\Microsoft Office\root\Office16\EXCEL.EXE"; // Upewnij się, że ścieżka jest poprawna
             // Link do pliku w OneDrive
-            string excelFileUrl = "https://d.docs.live.net/27187f2dd9885da3/PROBET/PROBET%20baza.xlsx";
-
-            // Otwieranie pliku Excel z linku
-            Process.Start(new ProcessStartInfo("cmd", $"/c start {excelFileUrl}") { UseShellExecute = true });
+            string excelFilePath = @"https://d.docs.live.net/27187f2dd9885da3/PROBET/PROBET%20baza.xlsx"; // Zmień na właściwą ścieżkę do pliku
+            // Otwieranie pliku Excel w klasycznej aplikacji Excel
+            Process.Start(new ProcessStartInfo(excelAppPath, excelFilePath) { UseShellExecute = true });
 
             // Konsola zamyka się automatycznie
             Environment.Exit(0);
